92ee8ed6e45c3e78d21a05e7e65f1892ae4c3b42
cbommas <chabom@amazon.com>
2018-08-24 12:43:22 -0700

    Added checks to ensure that the SDK doesn't crash if the received message is not convertible to String or JSONObject. Added a logger component to log the error message.

--- AWSAppSyncTests/AWSAppSyncTests.swift
+++ AWSAppSyncTests/AWSAppSyncTests.swift
@@ -1,6 +1,16 @@
 //
-//  AWSAppSyncTests.swift
-//  AWSAppSyncTests
+// Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
 //
 
 import XCTest
@@ -32,11 +42,15 @@ class AWSAppSyncTests: XCTestCase {
         let databaseURL = URL(fileURLWithPath:NSTemporaryDirectory()).appendingPathComponent(database_name)
         
         do {
+            let loggingClient:AWSAppSyncLogClient = AWSAppSyncLogClient()
+            loggingClient.setLoggingProvider(provider: AWSAppSyncDefaultLogProvider(logLevel: .verbose))
+            
             // Initialize the AWS AppSync configuration
             let appSyncConfig = try AWSAppSyncClientConfiguration(url: AppSyncEndpointURL,
                                                                   serviceRegion: AppSyncRegion,
                                                                   credentialsProvider: credentialsProvider,
-                                                                  databaseURL:databaseURL)
+                                                                  databaseURL:databaseURL,
+                                                                  loggingClient: loggingClient)
             // Initialize the AWS AppSync client
             appSyncClient = try AWSAppSyncClient(appSyncConfig: appSyncConfig)
             // Set id as the cache key for objects
