1e245b9ae7d7aa22d75a7bdc86a22a72856b2479
JohnRbk <troutspotting@gmail.com>
2018-09-07 20:29:16 -0400

    Allow Auth credentials to be fetched via callback(async), backwards compatible (#62)
    * Fetch auth token via callback
    
    FOr OIDC or Cognito authentication, auth tokens often expire and need to be re-generated. Using a callback follows the model also used by aws-mobile-appsync-sdk-js
    
    * removed log statement
    
    * Enabled async retrieval of auth tokens with backwards compatibility
    
    * Enabled async retrieval of auth tokens with backwards compatibility
    
    * removed extra blank lines
    
    * fixed copyright
    
    * prefer fatal error instead of silent failure where Async provider is used
    
    * ensure message is sent even when async provider is not used
    
    * removed extra blank lines
    
    * async token callback now accepts an error

--- AWSAppSyncClient/AWSAppSyncAuthProvider.swift
+++ AWSAppSyncClient/AWSAppSyncAuthProvider.swift
@@ -6,6 +6,17 @@
 import Foundation
 
 // For using OIDC based authorization, this protocol needs to be implemented and passed to configuration object.
+// Use this for cases where the OIDC token needs to be fetched asynchronously and requires a callback
+public protocol AWSOIDCAuthProviderAsync: AWSOIDCAuthProvider {
+    func getLatestAuthToken(_ callback: @escaping (String?, Error?) -> Void)
+}
+
+// For AuthProviders that use a callback, the getLatestAuthToken is defaulted to return an empty string
+extension AWSOIDCAuthProviderAsync {
+    public func getLatestAuthToken() -> String { fatalError("Callback method required") }
+}
+
+// For using OIDC based authorization, this protocol needs to be implemented and passed to configuration object.
 public protocol AWSOIDCAuthProvider {
     /// The method should fetch the token and return it to the client for using in header request.
     func getLatestAuthToken() -> String
