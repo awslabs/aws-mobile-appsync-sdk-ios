09197ff9dc34deeeed98484e71394d6bbf798782
Rohan Dubal <rohan.n.dubal@gmail.com>
2018-09-11 14:01:39 -0700

    Add a test app host for functional tests. Update tests to use a credentials file. Update travis config to use functional and unit tests.

---
+++ AWSAppSyncTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWSAppSyncTestApp
+//
+//  Created by Dubal, Rohan on 9/11/18.
+//  Copyright © 2018 Dubal, Rohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
