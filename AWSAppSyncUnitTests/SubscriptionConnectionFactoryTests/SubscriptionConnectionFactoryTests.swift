6ad6a2992d39c72a15f0a8800be616d5b26e39a8
Michael Law <1365977+lawmicha@users.noreply.github.com>
2020-02-18 12:53:25 -0800

    move test coverage from the deleted connection pool classes over to subscription factory tests

--- AWSAppSyncUnitTests/SubscriptionConnectionFactoryTests/SubscriptionConnectionFactoryTests.swift
+++ AWSAppSyncUnitTests/SubscriptionConnectionFactoryTests/SubscriptionConnectionFactoryTests.swift
@@ -11,27 +11,8 @@ import XCTest
 class SubscriptionConnectionFactoryTests: XCTestCase {
 
     let url = URL(string: "http://appsyncendpoint.com/graphql")!
+    let url2 = URL(string: "http://appsyncendpoint-2.com/graphql")!
 
-    /// Test the initial state of the factory
-    ///
-    /// - Given: An initiated factory object
-    /// - When:
-    ///    - I check the internal state
-    /// - Then:
-    ///    - States should be consistent
-    ///
-    func testInitialState() {
-        let connectionFactory = BasicSubscriptionConnectionFactory(url: url,
-                                                                   authType: .apiKey,
-                                                                   retryStrategy: .aggressive,
-                                                                   region: .USWest2,
-                                                                   apiKeyProvider: MockAPIKeyAuthProvider(),
-                                                                   cognitoUserPoolProvider: MockUserPoolsAuthProvider(),
-                                                                   oidcAuthProvider: MockUserPoolsAuthProvider(),
-                                                                   iamAuthProvider: MockIAMAuthProvider())
-        XCTAssertNotNil(connectionFactory.authInterceptor)
-    }
-    
     /// Test succesfull retrieval of IAM subscription connections
     ///
     /// - Given: A connection factory
@@ -49,10 +30,21 @@ class SubscriptionConnectionFactoryTests: XCTestCase {
                                                                    cognitoUserPoolProvider: nil,
                                                                    oidcAuthProvider: nil,
                                                                    iamAuthProvider: MockIAMAuthProvider())
+        let interceptor = connectionFactory.authTypeToInterceptor[.awsIAM]
         let connection = connectionFactory.connection(for: url,
                                                       authType: .awsIAM,
                                                       connectionType: .appSyncRealtime)
+        let provider = connectionFactory.endPointToProvider[url.absoluteString]
+        XCTAssertNotNil(provider)
+        XCTAssertNotNil(interceptor)
         XCTAssertNotNil(connection)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 1)
+
+        let connection2 = connectionFactory.connection(for: url2, authType: .awsIAM, connectionType: .appSyncRealtime)
+        let provider2 = connectionFactory.endPointToProvider[url2.absoluteString]
+        XCTAssertNotNil(connection2)
+        XCTAssertNotNil(provider2)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 2)
     }
     
     /// Test succesfull retrieval of UserPools subscription connections
@@ -65,17 +57,28 @@ class SubscriptionConnectionFactoryTests: XCTestCase {
     ///
     func testRetrieveUserPoolConnection() {
         let connectionFactory = BasicSubscriptionConnectionFactory(url: url,
-                                                                   authType: .apiKey,
+                                                                   authType: .amazonCognitoUserPools,
                                                                    retryStrategy: .aggressive,
                                                                    region: .USWest2,
                                                                    apiKeyProvider: nil,
-                                                                   cognitoUserPoolProvider: nil,
+                                                                   cognitoUserPoolProvider: MockUserPoolsAuthProvider(),
                                                                    oidcAuthProvider: nil,
-                                                                   iamAuthProvider: MockIAMAuthProvider())
+                                                                   iamAuthProvider: nil)
+        let interceptor = connectionFactory.authTypeToInterceptor[.amazonCognitoUserPools]
         let connection = connectionFactory.connection(for: url,
                                                       authType: .amazonCognitoUserPools,
                                                       connectionType: .appSyncRealtime)
+        let provider = connectionFactory.endPointToProvider[url.absoluteString]
+        XCTAssertNotNil(provider)
+        XCTAssertNotNil(interceptor)
         XCTAssertNotNil(connection)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 1)
+
+        let connection2 = connectionFactory.connection(for: url2, authType: .amazonCognitoUserPools, connectionType: .appSyncRealtime)
+        let provider2 = connectionFactory.endPointToProvider[url2.absoluteString]
+        XCTAssertNotNil(connection2)
+        XCTAssertNotNil(provider2)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 2)
     }
     
     /// Test succesfull retrieval of Apikey subscription connections
@@ -88,17 +91,28 @@ class SubscriptionConnectionFactoryTests: XCTestCase {
     ///
     func testRetrieveAPIKeyConnection() {
         let connectionFactory = BasicSubscriptionConnectionFactory(url: url,
-        authType: .apiKey,
-        retryStrategy: .aggressive,
-        region: .USWest2,
-        apiKeyProvider: MockAPIKeyAuthProvider(),
-        cognitoUserPoolProvider: MockUserPoolsAuthProvider(),
-        oidcAuthProvider: MockUserPoolsAuthProvider(),
-        iamAuthProvider: MockIAMAuthProvider())
+                                                                   authType: .apiKey,
+                                                                   retryStrategy: .aggressive,
+                                                                   region: nil,
+                                                                   apiKeyProvider: MockAPIKeyAuthProvider(),
+                                                                   cognitoUserPoolProvider: nil,
+                                                                   oidcAuthProvider: nil,
+                                                                   iamAuthProvider: nil)
+        let interceptor = connectionFactory.authTypeToInterceptor[.apiKey]
         let connection = connectionFactory.connection(for: url,
                                                       authType: .apiKey,
                                                       connectionType: .appSyncRealtime)
+        let provider = connectionFactory.endPointToProvider[url.absoluteString]
+        XCTAssertNotNil(provider)
+        XCTAssertNotNil(interceptor)
         XCTAssertNotNil(connection)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 1)
+
+        let connection2 = connectionFactory.connection(for: url2, authType: .apiKey, connectionType: .appSyncRealtime)
+        let provider2 = connectionFactory.endPointToProvider[url2.absoluteString]
+        XCTAssertNotNil(connection2)
+        XCTAssertNotNil(provider2)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 2)
     }
     
     /// Test succesfull retrieval of OIDC subscription connections
@@ -111,17 +125,28 @@ class SubscriptionConnectionFactoryTests: XCTestCase {
     ///
     func testRetrieveOIDCConnection() {
         let connectionFactory = BasicSubscriptionConnectionFactory(url: url,
-        authType: .apiKey,
-        retryStrategy: .aggressive,
-        region: .USWest2,
-        apiKeyProvider: MockAPIKeyAuthProvider(),
-        cognitoUserPoolProvider: MockUserPoolsAuthProvider(),
-        oidcAuthProvider: MockUserPoolsAuthProvider(),
-        iamAuthProvider: MockIAMAuthProvider())
+                                                                   authType: .oidcToken,
+                                                                   retryStrategy: .aggressive,
+                                                                   region: nil,
+                                                                   apiKeyProvider: nil,
+                                                                   cognitoUserPoolProvider: nil,
+                                                                   oidcAuthProvider: MockUserPoolsAuthProvider(),
+                                                                   iamAuthProvider: nil)
+        let interceptor = connectionFactory.authTypeToInterceptor[.oidcToken]
         let connection = connectionFactory.connection(for: url,
                                                       authType: .oidcToken,
                                                       connectionType: .appSyncRealtime)
+        let provider = connectionFactory.endPointToProvider[url.absoluteString]
+        XCTAssertNotNil(provider)
+        XCTAssertNotNil(interceptor)
         XCTAssertNotNil(connection)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 1)
+
+        let connection2 = connectionFactory.connection(for: url2, authType: .oidcToken, connectionType: .appSyncRealtime)
+        let provider2 = connectionFactory.endPointToProvider[url2.absoluteString]
+        XCTAssertNotNil(connection2)
+        XCTAssertNotNil(provider2)
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 2)
     }
     
     /// Test succesfull retrieval of multiple subscription connections together
@@ -157,5 +182,8 @@ class SubscriptionConnectionFactoryTests: XCTestCase {
         XCTAssertNotNil(apiKeyConnection)
         XCTAssertNotNil(oidcConnection)
         XCTAssertNotNil(userPoolConnection)
+
+        XCTAssertEqual(connectionFactory.endPointToProvider.count, 1)
+        XCTAssertEqual(connectionFactory.authTypeToInterceptor.count, 4)
     }
 }
