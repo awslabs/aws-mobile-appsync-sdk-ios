f2c637bd5398a55b1de98f788e3773f5686a580a
Michael law <1365977+lawmicha@users.noreply.github.com>
2020-07-10 15:33:31 -0700

    upgrade to 2.14.0 SDK and 1.2.0 realtime client

--- Pods/AWSMobileClient/AWSAuthSDK/Sources/AWSMobileClient/AWSUserPoolOperationsHandler.swift
+++ Pods/AWSMobileClient/AWSAuthSDK/Sources/AWSMobileClient/AWSUserPoolOperationsHandler.swift
@@ -63,13 +63,42 @@ AWSCognitoUserPoolInternalDelegate {
     var authHelperDelegate: UserPoolAuthHelperlCallbacks?
     var customAuthHandler: AWSUserPoolCustomAuthHandler?
     internal static let sharedInstance: UserPoolOperationsHandler = UserPoolOperationsHandler()
+
+    static var serviceConfiguration: CognitoServiceConfiguration? = nil
+
     
     public override init() {
         super.init()
         if (AWSInfo.default().defaultServiceInfo("CognitoUserPool") != nil) {
-            self.userpoolClient = AWSCognitoIdentityUserPool.default()
+            self.userpoolClient = self.getUserPoolClient()
             self.userpoolClient?.delegate = self
         }
+    }
+
+    private func getUserPoolClient() -> AWSCognitoIdentityUserPool {
+        
+        guard let serviceConfig = UserPoolOperationsHandler.serviceConfiguration?.userPoolServiceConfiguration else {
+            return AWSCognitoIdentityUserPool.default()
+        }
+
+        // Check if a AWSCognitoIdentityUserPool is already registered with the given key.
+        let clientKey = "CognitoUserPoolKey"
+        if let client = AWSCognitoIdentityUserPool.init(forKey: clientKey) {
+            return client
+        }
+
+        // If the AWSCognitoIdentityUserPool is not registered, register it and then return the same object.
+        let serviceInfo = AWSInfo.default().defaultServiceInfo("CognitoUserPool")
+        let userPoolConfig = AWSCognitoIdentityUserPool.buildConfiguration(serviceInfo)
+        AWSCognitoIdentityUserPool.register(with: serviceConfig,
+                                            userPoolConfiguration: userPoolConfig,
+                                            forKey: clientKey)
+        if let client =  AWSCognitoIdentityUserPool.init(forKey: clientKey) {
+            return client
+        }
+
+        /// Fall back to the default client if the client was not registered correctly.
+        return AWSCognitoIdentityUserPool.default()
     }
     
     internal func startPasswordAuthentication() -> AWSCognitoIdentityPasswordAuthentication {
