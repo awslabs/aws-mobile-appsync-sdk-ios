d0ebd43c3c6fce845a4e389a460e2948bbcc4ede
Tim Schmelter <schmelte@amazon.com>
2020-07-28 16:00:54 -0700

    chore: Upgrade dependency versions

--- Pods/AppSyncRealTimeClient/AppSyncRealTimeClient/Connection/SubscriptionItem.swift
+++ Pods/AppSyncRealTimeClient/AppSyncRealTimeClient/Connection/SubscriptionItem.swift
@@ -22,9 +22,9 @@ public struct SubscriptionItem {
     // Subscription related events will be send to this handler.
     let subscriptionEventHandler: SubscriptionEventHandler
 
-    init(requestString: String,
-         variables: [String: Any?]?,
-         eventHandler: @escaping SubscriptionEventHandler) {
+    public init(requestString: String,
+                variables: [String: Any?]?,
+                eventHandler: @escaping SubscriptionEventHandler) {
 
         self.identifier = UUID().uuidString
         self.variables = variables
