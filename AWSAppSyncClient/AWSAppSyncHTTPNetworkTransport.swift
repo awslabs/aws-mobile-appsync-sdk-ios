80300f478227fcc9118ba82819d2394ce8be50ba
Diego Costantino <diegoco@amazon.com>
2022-02-24 13:06:39 -0800

    Release SDK version 3.5.0

--- AWSAppSyncClient/AWSAppSyncHTTPNetworkTransport.swift
+++ AWSAppSyncClient/AWSAppSyncHTTPNetworkTransport.swift
@@ -191,7 +191,7 @@ public class AWSAppSyncHTTPNetworkTransport: AWSNetworkTransport {
         request.httpMethod = "POST"
         request.setValue(NSDate().aws_stringValue(AWSDateISO8601DateFormat2), forHTTPHeaderField: "X-Amz-Date")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("aws-sdk-ios/3.4.3 AppSyncClient", forHTTPHeaderField: "User-Agent")
+        request.setValue("aws-sdk-ios/3.5.0 AppSyncClient", forHTTPHeaderField: "User-Agent")
         addDeviceId(request: &request)
     }
 
