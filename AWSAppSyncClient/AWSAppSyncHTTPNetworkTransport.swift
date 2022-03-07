3a37ac54d2b6aac23bb6427670c05fe3028a895a
Michael Law <1365977+lawmicha@users.noreply.github.com>
2022-03-07 12:49:41 -0500

    Release SDK version 3.6.0

--- AWSAppSyncClient/AWSAppSyncHTTPNetworkTransport.swift
+++ AWSAppSyncClient/AWSAppSyncHTTPNetworkTransport.swift
@@ -192,7 +192,7 @@ public class AWSAppSyncHTTPNetworkTransport: AWSNetworkTransport {
         request.setValue(UUID().uuidString, forHTTPHeaderField: "x-choco-correlation-id")
         request.setValue(NSDate().aws_stringValue(AWSDateISO8601DateFormat2), forHTTPHeaderField: "X-Amz-Date")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("aws-sdk-ios/3.5.0 AppSyncClient", forHTTPHeaderField: "User-Agent")
+        request.setValue("aws-sdk-ios/3.6.0 AppSyncClient", forHTTPHeaderField: "User-Agent")
         addDeviceId(request: &request)
     }
 
