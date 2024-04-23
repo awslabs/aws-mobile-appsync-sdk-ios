c37e6f0892f72ef2c7766b6adb6e28c2f888edd6
Di Wu <github@wudi.me>
2024-04-23 21:31:47 +0000

    chore: release 3.7.0 (#594)

--- AWSAppSyncClient/AWSAppSyncHTTPNetworkTransport.swift
+++ AWSAppSyncClient/AWSAppSyncHTTPNetworkTransport.swift
@@ -191,7 +191,7 @@ public class AWSAppSyncHTTPNetworkTransport: AWSNetworkTransport {
         request.httpMethod = "POST"
         request.setValue(NSDate().aws_stringValue(AWSDateISO8601DateFormat2), forHTTPHeaderField: "X-Amz-Date")
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("aws-sdk-ios/3.6.5 AppSyncClient", forHTTPHeaderField: "User-Agent")
+        request.setValue("aws-sdk-ios/3.7.0 AppSyncClient", forHTTPHeaderField: "User-Agent")
         addDeviceId(request: &request)
     }
 
