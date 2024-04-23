316d9a7a9738c21d43bf15e1235f587cbe7a509f
Di Wu <diwud@amazon.com>
2024-04-23 10:33:48 -0700

    update version is podspec and Pakcage.swift

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
         .package(
             name: "AppSyncRealTimeClient",
             url: "https://github.com/aws-amplify/aws-appsync-realtime-client-ios.git",
-            from: "3.0.0"
+            .upToNextMinor(from: "3.1.0")
         ),
         .package(
             url: "https://github.com/stephencelis/SQLite.swift.git",
-            from: "0.12.0"
+            .upToNextMinor(from: "0.12.0")
         )
     ],
     targets: [
